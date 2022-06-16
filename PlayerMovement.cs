7f0165a6e7f79548033e7080a848cdaf65847ba2
OM8890 <107632911+OM8890@users.noreply.github.com>
2022-06-16 17:39:32 +0200

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,43 +1,43 @@
 using UnityEngine;
 using UnityEngine.System;
 
-    private bool cancellingGrounded;
+private bool cancellingGrounded;
 
-    /// <summary>
-    /// Handle ground detection
-    /// </summary>
-    private void OnCollisionStay(Collision other)
+/// <summary>
+/// Handle ground detection
+/// </summary>
+private void OnCollisionStay(Collision other)
+{
+    //Make sure we are only checking for walkable layers
+    int layer = other.gameObject.layer;
+    if (whatIsGround != (whatIsGround | (1 << layer))) return;
+
+    //Iterate through every collision in a physics update
+    for (int i = 0; i < other.contactCount; i++)
     {
-        //Make sure we are only checking for walkable layers
-        int layer = other.gameObject.layer;
-        if (whatIsGround != (whatIsGround | (1 << layer))) return;
-
-        //Iterate through every collision in a physics update
-        for (int i = 0; i < other.contactCount; i++)
+        Vector3 normal = other.contacts[i].normal;
+        //FLOOR
+        if (IsFloor(normal))
         {
-            Vector3 normal = other.contacts[i].normal;
-            //FLOOR
-            if (IsFloor(normal))
-            {
-                grounded = true;
-                cancellingGrounded = false;
-                normalVector = normal;
-                CancelInvoke(nameof(StopGrounded));
-            }
-        }
-
-        //Invoke ground/wall cancel, since we can't check normals with CollisionExit
-        float delay = 3f;
-        if (!cancellingGrounded)
-        {
-            cancellingGrounded = true;
-            Invoke(nameof(StopGrounded), Time.deltaTime * delay);
+            grounded = true;
+            cancellingGrounded = false;
+            normalVector = normal;
+            CancelInvoke(nameof(StopGrounded));
         }
     }
 
-    private void StopGrounded()
+    //Invoke ground/wall cancel, since we can't check normals with CollisionExit
+    float delay = 3f;
+    if (!cancellingGrounded)
     {
-        grounded = false;
+        cancellingGrounded = true;
+        Invoke(nameof(StopGrounded), Time.deltaTime * delay);
     }
+}
+
+private void StopGrounded()
+{
+    grounded = false;
+}
 
 }
